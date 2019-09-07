286cd3c9bca6369d8588b70dac63a7253809b961
Vitor Emanuel <veslima3@gmail.com>
2019-09-07 19:34:36 -0300

    Update actions.js

--- player2/js/actions.js
+++ player2/js/actions.js
@@ -2,4 +2,6 @@ let toggleNavStatus = false;
 
 let toggleNav = function(){
   let getSidebar = document.querySelector(".nav-sidebar");
+  let getSidebarUl = document.querySelector(".nav-sidebar ul");
+  let getSidebarLinks = document.querySelectorAll(".nav-sidebar a");
 }
